97a9c2eec1242afee2309cc446da86be17b1075d
super <97206982+SuperMo0@users.noreply.github.com>
2025-08-19 18:45:54 +0300

    add function change dimension

--- script.js
+++ script.js
@@ -2,18 +2,24 @@ let grid = document.querySelector(".container");
 let change = document.querySelector(".change");
 grid_side = 850;
 
+grid.ondragstart = () => { return false };
+grid.ondrop = () => { return false };
+
+
 function build(current_size) {
 
     let cell_width, cell_height;
-    cell_height = cell_width = grid_side / current_size;
+    cell_height = cell_width = grid_side / current_size - 0.0000001;
     let box = document.createElement("div");
     box.style["width"] = cell_width + 'px';
     box.style['height'] = cell_height + 'px';
     box.style['border'] = '1px solid black';
     box.style['box-sizing'] = 'border-box';
-    grid.addEventListener("mouseover", function (e) { if (e.target.className == 'container') return; e.target.style['background-color'] = 'black'; });
+    grid.addEventListener("mouseover", function (e) {
+        if (e.target.className == 'container' || e.buttons != 1) return; e.target.style['background-color'] = 'black';
+    });
     for (let i = 0; i < current_size * current_size; i++) {
-        let new_box = box.cloneNode(true);
+        let new_box = box.cloneNode();
         grid.appendChild(new_box)
     }
 }
@@ -27,13 +33,15 @@ function empty_grid() {
 }
 
 function take_size() {
-    let value = prompt('please enter grid side size');
+    let value = prompt('please enter grid side size (one number at most 100 )');
+
+    if (value == null || value == '') return;
     empty_grid();
     build(value);
 }
 
 change.addEventListener("click", take_size);
-build(16);
+build(70);
 
 
 
