bce244d2cac7a40ca3c27ab7d0cf3440a38b5f8f
super <97206982+SuperMo0@users.noreply.github.com>
2025-08-19 17:35:21 +0300

    add change dimension and draw effect

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let grid = document.querySelector(".container");
+let change = document.querySelector(".change");
 grid_side = 850;
 
 function build(current_size) {
@@ -10,16 +11,32 @@ function build(current_size) {
     box.style['height'] = cell_height + 'px';
     box.style['border'] = '1px solid black';
     box.style['box-sizing'] = 'border-box';
-    // box.style['background-color'] = 'black'
-    // box.style['flex'] = '0';
-
+    grid.addEventListener("mouseover", function (e) { if (e.target.className == 'container') return; e.target.style['background-color'] = 'black'; });
     for (let i = 0; i < current_size * current_size; i++) {
         let new_box = box.cloneNode(true);
         grid.appendChild(new_box)
     }
 }
 
+function empty_grid() {
+
+    let list = document.querySelectorAll('.container div');
+    for (let i = 0; i < list.length; i++) {
+        list[i].remove();
+    }
+}
+
+function take_size() {
+    let value = prompt('please enter grid side size');
+    empty_grid();
+    build(value);
+}
+
+change.addEventListener("click", take_size);
 build(16);
+
+
+
 
 
 
